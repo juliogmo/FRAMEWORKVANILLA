9c4deeb2070a5b821e34a94dc5b6593cf2e87d86
juliogmo <147669951+juliogmo@users.noreply.github.com>
2023-10-21 14:19:38 -0600

    Update codigo.js

--- Cifrado_escitala_vgj/codigo.js
+++ Cifrado_escitala_vgj/codigo.js
@@ -1,34 +1,65 @@
-function cifrarmensaje() {
-    const textoOriginal = document.getElementById("textoOriginal").value;
-    const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+function cifrarMensaje() {
+    const mensaje = document.getElementById("mensaje").value;
+    const clave = parseInt(document.getElementById("clave").value);
 
-    let textoCifrado = "";
+    const mensajeCifrado = cifrarEscitala(mensaje, clave);
 
-    for (let i = 0; i < desplazamiento; i++) {
-        for (let j = i; j < textoOriginal.length; j += desplazamiento) {
-            textoCifrado += textoOriginal[j];
-        }
-    }
-
-    document.getElementById("textoCifrado").textContent = textoCifrado;
+    document.getElementById("resultado1").textContent = mensajeCifrado;
 }
 
-function descifrarmensaje() {
-    const textoOriginal = document.getElementById("textoOriginal").value;
-    const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+function descifrarMensaje() {
+    const mensaje = document.getElementById("mensaje").value;
+    const clave = parseInt(document.getElementById("clave").value);
 
-    const filas = Math.ceil(textoOriginal.length / desplazamiento);
-    const textodecifrado = new Array(filas).fill("");
+    const mensajeDescifrado = descifrarEscitala(mensaje, clave);
+
+    document.getElementById("resultado2").textContent = mensajeDescifrado;
+}
+
+function cifrarEscitala(mensaje, clave) {
+    const filas = Math.ceil(mensaje.length / clave);
+    let matriz = Array.from({ length: filas }, () => Array(clave).fill(" "));
 
     let index = 0;
     for (let i = 0; i < filas; i++) {
-        for (let j = 0; j < desplazamiento; j++) {
-            if (index < textoOriginal.length) {
-                textodecifrado[i] += textoOriginal[index];
+        for (let j = 0; j < clave; j++) {
+            if (index < mensaje.length) {
+                matriz[i][j] = mensaje[index];
                 index++;
             }
         }
     }
 
-    document.getElementById("textoDescifrado").textContent = textodecifrado.join("");
+    let mensajeCifrado = "";
+    for (let j = 0; j < clave; j++) {
+        for (let i = 0; i < filas; i++) {
+            mensajeCifrado += matriz[i][j];
+        }
+    }
+
+    return mensajeCifrado;
+}
+
+function descifrarEscitala(mensaje, clave) {
+    const filas = Math.ceil(mensaje.length / clave);
+    let matriz = Array.from({ length: filas }, () => Array(clave).fill(" "));
+
+    let index = 0;
+    for (let j = 0; j < clave; j++) {
+        for (let i = 0; i < filas; i++) {
+            if (index < mensaje.length) {
+                matriz[i][j] = mensaje[index];
+                index++;
+            }
+        }
+    }
+
+    let mensajeDescifrado = "";
+    for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < clave; j++) {
+            mensajeDescifrado += matriz[i][j];
+        }
+    }
+
+    return mensajeDescifrado.trim();
 }
